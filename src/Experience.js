b6fca203b21241024f2ee705bf9340d2e4efa303
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-11 19:40:46 +0200

    Initial commit

---
+++ src/Experience.js
@@ -0,0 +1,17 @@
+import { OrbitControls } from '@react-three/drei'
+
+export default function Experience()
+{
+    return <>
+
+        <color args={ [ '#201919' ] } attach="background" />
+
+        <OrbitControls makeDefault />
+
+        <mesh scale={ 1.5 }>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
+
+    </>
+}
